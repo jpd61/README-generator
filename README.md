f1f0e5e0cfec3c2ec136441475dbb6b6e749f1cb
jpd61 <jpd@dewoodyoil.com>
2020-05-10 15:06:35 -0500

    testing and updates

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 
 # README.md_Generator
 Using Node.js, generate a template README.md file with users input to generate the README.
-jose
-        
-## Description    
-jose
+test title
+
         
 ## Table of Contents
 - [Description](#description)
@@ -17,27 +15,37 @@ jose
 - [Contributors](#contributors)
 - [Acknowledgments](#acknowledgments)
 - [Images](#images)
-- [CopyRight](#copyright)
+- [Copyright](#copyright)
         
+## Description    
+test desc
+
 ### Installation
-jose
+test install
+
 ### Usage
-jose
+school
+
 ### Built With
 * [VScode](https://code.visualstudio.com/)
 * [Gitbash](https://gitforwindows.org/)
+
 ### Questions
-jose
+test issue
         
 ### LicenseMIT License
-### Contributors
-jose
-### Acknowledgments
-#### Badges
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-        
+
+### Contributors
+tet cont
+
+### Acknowledgments
+undefined
+
 ### GitHub Username
+jpd61
 my_email@github.com
+
 ### Avatar Image
 <img src="https://avatars3.githubusercontent.com/u/61440109?v=4"/>
 /n* Copyright 2020 &copy;
--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -4,9 +4,7 @@ function generateReadme(answers) {
 # README.md_Generator
 Using Node.js, generate a template README.md file with users input to generate the README.
 ${answers.projectTitle}
-        
-## Description    
-${answers.description}
+
         
 ## Table of Contents
 - [Description](#description)
@@ -19,27 +17,37 @@ ${answers.description}
 - [Contributors](#contributors)
 - [Acknowledgments](#acknowledgments)
 - [Images](#images)
-- [CopyRight](#copyright)
+- [Copyright](#copyright)
         
+## Description    
+${answers.description}
+
 ### Installation
 ${answers.installation}
+
 ### Usage
 ${answers.usage}
+
 ### Built With
 * [VScode](https://code.visualstudio.com/)
 * [Gitbash](https://gitforwindows.org/)
+
 ### Questions
 ${answers.questions}
         
 ### License${answers.license}
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
 ### Contributors
 ${answers.contributing}
+
 ### Acknowledgments
-#### Badges
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-        
+${answers.acknowledgements}
+
 ### GitHub Username
+${answers.username}
 ${answers.email}
+
 ### Avatar Image
 <img src="${answers.avatar_url}"/>
 /n* Copyright 2020 &copy;
